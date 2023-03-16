61762e0d64b91cb6d12fbcf63c50e564cd2b9847
aozou99 <hellostacysmom@gmail.com>
2023-03-17 08:23:31 +0900

    fix

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ with open('youtube.list', 'r') as f:
     urls = f.read().split("\n")
 
 for url in urls:
+    if not url.startswith('http'):
+        continue
     meta = ydl.download(url)
     whisper.call(
         f"./output/{meta['id']}/audio.mp3", meta['id'], meta['title'])
